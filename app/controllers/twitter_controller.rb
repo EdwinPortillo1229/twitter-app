7355578b0090414782eacb282d61c6769b97ce40
Edwin Portillo <edwinportillo@Edwins-MacBook-Pro.local>
2023-12-10 18:53:50 -0500

    whatever

---
+++ app/controllers/twitter_controller.rb
@@ -0,0 +1,19 @@
+class TwitterController < ApplicationController
+
+    API_KEY = "DGjDC1KL6lRucYDGN5kjGv0x0"
+    API_KEY_SECRET = "IKbnwyeyuFtkWioL4SbPU4CxiScjNwUikXodx31QSv4dPZ7OBs"
+    BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAD8brQEAAAAA%2FSQesugga3E1gnlmtTo6UnLCLks%3DfteXMhSYQ8CmgrQHII19eWYrPYCM1yWAlUnFA5Iuo768UEyWjJ"
+    ACCESS_TOKEN = "2350164533-JuB5JxdZABYVi6R8Q1hJolxutCF8ipMPSbYu5cQ"
+    ACCESS_TOKEN_SECRET = "CoHrNkLnNdRhbmGlas2B8qpTh0HgMA69vThE1s039sLVe"
+
+    CLIENT_ID = "ZXgxOWU1Y2x1djB3cTdQNkhJQW86MTpjaQ"
+    CLIENT_SECRET = "2gGmvEs3TmZiEjs2RJ75mtHjr9JtOFStcVqJB0IcDzDJwGFGt4"
+    def link
+
+    end
+
+    def linked
+
+    end
+
+end
